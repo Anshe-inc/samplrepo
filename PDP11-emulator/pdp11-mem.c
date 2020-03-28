c0162da05af66949ca9b22d3fd197540995b1cb6
Anshe <shcherbakov.andrei@phystech.edu>
2020-03-28 22:44:15 +0300

    first file added

---
+++ PDP11-emulator/pdp11-mem.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef char byte;				//8-bit
+typedef unsigned short word;	//16-bit
+typedef word Address;			//16-bit
+
+#define MEMSIZE (64*1024)
+
+byte mem[MEMSIZE];
+
+void b_write(Address addr, byte b);
+byte b_read(Address addr);
+void w_write(Address addr, byte b);
+word w_write(Address addr);
+
