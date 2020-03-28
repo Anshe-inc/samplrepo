f5358e99f029d4fed9336f4b372fef9aeb0911a9
Anshe <shcherbakov.andrei@phystech.edu>
2020-03-29 00:09:13 +0300

    creatin' test function instead of writing in 'main'

--- PDP11-emulator/pdp11-mem.c
+++ PDP11-emulator/pdp11-mem.c
@@ -14,25 +14,27 @@ byte b_read(Address addr);
 void w_write(Address addr, word b);
 word w_read(Address addr);
 
-int main() {
+void testing() {
 	byte b0 = 0xa0;									//b0 = decimal(160)
 	b_write(2, b0);									//write to address 0x2
 
 	byte b_res = b_read(2);							//read from address 0x2
 
 	printf("%02hhx = %02hhx\n", b0, b_res);			//print the result and compare
-	
+
 	//tests for words
 	Address a = 4;
 	b0 = 0x0a;
 	byte b1 = 0x0b;
-	word b = 0xcb0a;
+	word b = 0x0b0a;
 	b_write(a, b0);
-	b_write(a+1, b1);
+	b_write(a + 1, b1);
 	word w_res = w_read(a);
 	printf("word/bytes \t %04hx = %02hhx%02hhx\n", w_res, b1, b0);
 	assert(w_res == b);
-
+}
+int main(){
+	testing();
 	return 0;
 }
 
