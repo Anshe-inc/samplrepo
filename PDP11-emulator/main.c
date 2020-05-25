431d7dc94f10fc92d80b96592a1ec1d339ae30ac
Anshe-inc <shcherbakov.andrei@phystech.edu>
2020-05-25 16:08:04 +0300

    -t is working now

--- PDP11-emulator/main.c
+++ PDP11-emulator/main.c
@@ -1,11 +1,16 @@
 #include "main.h"
 #include "test.h"
 
+byte is_traced = 0;
+
 
 
 int main(int argc, char * argv[]){
 	if(argc > 1){									//tests if path was written
-		test_load(argv[1]);							//to command line(not correct)
+		if(strstr("-t", argv[1]) != NULL){
+			is_traced = 1;
+		}
+		test_load(argv[argc - 1]);							//to command line(not correct)
 	}
 	else{
 		assert(argc > 1);
